7031d8ee8d1290447fb89d6e2f04cc3b0916eb07
Renish B <renish.bhaskaran@globalenglish.com>
2018-11-07 15:07:14 +0530

    Added basic layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Footer from './components/layouts/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React App</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
