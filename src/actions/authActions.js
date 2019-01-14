5b660176fe34d797d0fe3b9119b90b4b83d67d45
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-14 20:34:27 +0530

    login user and route it to dashboard

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utilities/setAuthToken';
+import jwt_decode from 'jwt-decode'
 
 // Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -13,3 +15,38 @@ export const registerUser = (userData, history) => dispatch => {
           });
       });
 }
+
+// Login user
+export const loginUser = userData => dispatch => {
+    //Login request to the server
+    axios.post('http://localhost:5000/api/users/login', userData)
+        .then(res => {
+            //Get the token
+            const { token } = res.data;
+
+            //Save token to the localstorage
+            localStorage.setItem('jwtToken', token);
+
+            //Set token to the Auth Header
+            setAuthToken(token);
+
+            //Decode the token and get user data
+            const decodedUserData = jwt_decode(token);
+
+            //Set the current user state
+            dispatch(setCurrentUser(decodedUserData));
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            });
+        });
+};
+
+export const setCurrentUser = decodedUserData => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decodedUserData
+    }
+};
